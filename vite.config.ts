1b016316a26ac006d125e4a0d2e0574fc86974bf
Jonathan Sheely <jsheely@thirtytech.net>
2023-02-14 14:58:19 -0500

    Suffix name

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 import path from 'path'
 import react from '@vitejs/plugin-react'
-import { name } from './package.json'
+import { name as packageName} from './package.json'
+
+const name = packageName.substring(packageName.lastIndexOf('/') + 1);
 
 // https://vitejs.dev/config/
 export default defineConfig({
