563f3ad1c9b9e672deb07239ce9d68fb0a3203a8
Jonathan Sheely <jsheely@thirtytech.net>
2023-03-28 17:25:49 -0400

    Flipped the overload order.
    Allows the primary types scenario to fire immediately

--- src/lib/yasml.tsx
+++ src/lib/yasml.tsx
@@ -27,10 +27,11 @@ function yasml<Props, Value extends StateResult>(
   State: (props: Props) => Value
 ) {
   const contexts = new Map<keyof Value, Context<unknown>>();
+  let _trappedState: Value = {} as Value;
   const Provider: FC<PropsWithChildren<Props>> = ({ children, ...props }) => {
     let element = children as ReactElement;
     const value = State(props as Props);
-
+    _trappedState = value;
     if (typeof value !== "object") {
       throw new Error("The state must return an object.");
     }
@@ -58,12 +59,24 @@ function yasml<Props, Value extends StateResult>(
     Provider.displayName = State.name;
   }
 
-  function useSelector<T extends Array<keyof Value>>(
+  function useSelector<T extends (keyof Value)[]>(
     ...keys: T
-  ): T["length"] extends 0 ? Value : Pick<Value, T[number]> {
+    ): T["length"] extends 0 ? Value : Pick<Value, T[number]>;
+    function useSelector<T extends (value: Value) => Partial<Value>>(
+      selector: T
+    ): ReturnType<T>;
+  function useSelector<T extends (keyof Value)[]>(
+    ...keys: T | [(value: Value) => Partial<Value>]
+  ): T | T["length"] extends 0 ? Value : Pick<Value, T[number]> {
+    let innerKeys = [] as (keyof Value)[];
+    if (typeof keys[0] === "function") {
+      innerKeys = Object.keys(keys[0](_trappedState));
+    } else {
+      innerKeys;
+    }
     const result = {} as { [key in T[number]]: Value[key] };
 
-    if (keys.length === 0) {
+    if (innerKeys.length === 0) {
       contexts.forEach((context) => {
         const value = useContext(context) as Value[keyof Value];
         if (isDev && value === NO_PROVIDER) {
@@ -73,7 +86,7 @@ function yasml<Props, Value extends StateResult>(
         result[name] = value;
       });
     } else {
-      keys.forEach((key) => {
+      innerKeys.forEach((key) => {
         const context = contexts.get(key as T[number]) as Context<unknown>;
         const value = useContext(context) as Value[T[number]];
         if (isDev && value === NO_PROVIDER) {
