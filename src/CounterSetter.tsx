fa8b25f57fe99d3ac1353825dacd8e85c264664c
Jonathan Sheely <jsheely@thirtytech.net>
2023-03-28 17:25:59 -0400

    Example of usage of selector

--- src/CounterSetter.tsx
+++ src/CounterSetter.tsx
@@ -1,6 +1,7 @@
 import { useGlobalState } from "./state/MyState";
 
 export function CounterSetter() {
-  const { increment } = useGlobalState('increment');
+  const { increment } = useGlobalState("increment", "counter");
+  const state = useGlobalState((state) => ({ counter: state.counter }));
   return <button onClick={() => increment()}>Increment</button>;
 }
