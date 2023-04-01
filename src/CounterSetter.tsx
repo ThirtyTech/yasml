4c6780c56876102b998bc0a3a7d51f5d219b6457
Jonathan Sheely <jsheely@thirtytech.net>
2023-04-01 14:08:58 -0400

    Typing issue

--- src/CounterSetter.tsx
+++ src/CounterSetter.tsx
@@ -3,5 +3,5 @@ import { useGlobalState } from "./state/MyState";
 export function CounterSetter() {
   const { increment } = useGlobalState();
   const state = useGlobalState((state) => ({ counter: state.counter }));
-  return <button onClick={() => increment()}>Increment</button>;
+  return <button onClick={() => increment()}>Increment {state.counter}</button>;
 }
