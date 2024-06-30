9fc27ad717b670697ed9af95334a9503601b4788
Jonathan Sheely <jsheely@thirtytech.net>
2024-06-30 10:55:30 -0400

    Added new context caching layer.
    It seems that useSelector can be called before Provider is set during rapid hot reloading.

--- src/lib/yasml.tsx
+++ src/lib/yasml.tsx
@@ -23,31 +23,45 @@ function displayWarning(name: string | undefined) {
   console.warn(warnMessage);
 }
 
+const _cachedContext = new Map<string, Map<unknown, Context<unknown>>>();
 function yasml<Props, Value extends StateResult>(
   State: (props: Props) => Value
 ) {
-  const contexts = new Map<keyof Value, Context<unknown>>();
-  let _trappedState: Value = {} as Value;
+  const contexts =
+    isDev && _cachedContext.get(State.name)
+      ? (_cachedContext.get(State.name) as Map<keyof Value, Context<unknown>>)
+      : new Map<keyof Value, Context<unknown>>();
+  let _cachedState: Value = {} as Value;
   const Provider: FC<PropsWithChildren<Props>> = ({ children, ...props }) => {
     let element = children as ReactElement;
-    const value = State(props as Props);
-    _trappedState = value;
-    if (typeof value !== "object") {
+    const stateValues = State(props as Props);
+    _cachedState = stateValues;
+    if (typeof stateValues !== "object") {
       throw new Error("The state must return an object.");
     }
 
-    Object.keys(value)
-      .reverse()
+    Object.keys(stateValues)
+      .sort((a, b) => a.localeCompare(b))
       .forEach((key) => {
         if (!contexts.has(key as keyof Value)) {
           const context = createContext(NO_PROVIDER) as Context<unknown>;
           context.displayName = key;
           contexts.set(key as keyof Value, context);
+          if (isDev) {
+            const cache = _cachedContext.get(State.name);
+            if (cache) {
+              cache.set(key, context);
+            } else {
+              _cachedContext.set(State.name, new Map([[key, context]]));
+            }
+          }
         }
         const context = contexts.get(key as keyof Value);
         if (context) {
           element = (
-            <context.Provider value={value[key]}>{element}</context.Provider>
+            <context.Provider value={stateValues[key]}>
+              {element}
+            </context.Provider>
           );
         }
       });
@@ -72,7 +86,7 @@ function yasml<Props, Value extends StateResult>(
     let _cacheFuncResult = {} as Partial<Value>;
     const isFunction = typeof keys[0] === "function";
     if (typeof keys[0] === "function") {
-      _cacheFuncResult = keys[0](_trappedState);
+      _cacheFuncResult = keys[0](_cachedState);
       contextKeys = Object.keys(_cacheFuncResult);
     } else {
       contextKeys = keys as (keyof Value)[];
@@ -81,22 +95,46 @@ function yasml<Props, Value extends StateResult>(
 
     if (contextKeys.length === 0) {
       contexts.forEach((context) => {
+        const name = context.displayName as keyof Value;
         const value = useContext(context) as Value[keyof Value];
+
         if (isDev && value === NO_PROVIDER) {
           displayWarning(context.displayName);
         }
-        const name = context.displayName as keyof Value;
+
+        // Stores last known value from context. Used for not reloading
+        if (isDev && value !== NO_PROVIDER) {
+          _cachedState[name] = value;
+        }
         result[name] = value;
+
+        // Handles condition during hot reload where value is lost
+        if (isDev && value === NO_PROVIDER && name in _cachedState) {
+          result[name] = _cachedState[name];
+        }
       });
     } else {
       contextKeys.forEach((key) => {
         const context = contexts.get(key as T[number]) as Context<unknown>;
+
         if (context) {
           const value = useContext(context) as Value[T[number]];
+
           if (isDev && value === NO_PROVIDER) {
             displayWarning(context.displayName);
           }
+
+          // Stores last known value from context. Used for not reloading
+          if (isDev && value !== NO_PROVIDER) {
+            _cachedState[key] = value;
+          }
+
           result[key] = value;
+
+          // Handles condition during hot reload where value is lost
+          if (isDev && value === NO_PROVIDER && key in _cachedState) {
+            result[key] = _cachedState[key];
+          }
         } else if (isFunction) {
           result[key] = _cacheFuncResult[key] as Value[T[number]];
         } else if (typeof key === "string") {
