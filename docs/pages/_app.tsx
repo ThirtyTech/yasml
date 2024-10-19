1aa08a3a9ada323cb7622099fb928bdbab56f242
Jonathan Sheely <jsheely@thirtytech.net>
2024-10-19 16:33:08 -0400

    Updated all the docs packages and semver

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -5,6 +5,7 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ?? ""} />
+      {/* Note: No idea why this type is broken. Works in a new app. Workspace related? Babel+Core in the middle. */}
       {/* @ts-ignore */}
       <Component {...pageProps} />
     </>
