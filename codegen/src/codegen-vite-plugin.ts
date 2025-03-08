e2c998d18fdb5a449cbb6779cd1fb29679f24db7
Jonathan Sheely <jsheely@thirtytech.net>
2025-03-08 17:49:22 -0500

    Prefer to not set defaults by convention

--- codegen/src/codegen-vite-plugin.ts
+++ codegen/src/codegen-vite-plugin.ts
@@ -4,34 +4,13 @@ import * as parser from "@typescript-eslint/parser";
 import rule from "@thirtytech/eslint-plugin-yasml/dist/rules/matchExportParameters";
 
 type CodeGeneratorYasmlPluginPatternOptions = {
-  allFiles?: never;
-  patterns?: string | string[] | (RegExp | RegExp[]);
+  patterns?: string[] | RegExp[];
 };
-
-type CodeGeneratorYasmlPluginAllFilesOptions = {
-  patterns?: never;
-  allFiles?: true;
-};
-
-type CodeGeneratorYasmlPluginOptions =
-  | CodeGeneratorYasmlPluginPatternOptions
-  | CodeGeneratorYasmlPluginAllFilesOptions;
 
 export function codeGeneratorYasmlPlugin(
-  options: CodeGeneratorYasmlPluginOptions
+  { patterns }: CodeGeneratorYasmlPluginPatternOptions
 ): PluginOption {
   const linter = new Linter({ cwd: process.cwd(), configType: "eslintrc" });
-  let patterns: string[] | RegExp[] = [];
-
-  if ("patterns" in options && typeof options.patterns === "string") {
-    patterns = [options.patterns];
-  } else if ("patterns" in options && options.patterns instanceof RegExp) {
-    patterns = [options.patterns];
-  } else if ("allFiles" in options) {
-    patterns = [];
-  } else if (!patterns && !options.allFiles) {
-    patterns = [/\.state/g, /\.hooks/g];
-  }
 
   linter.defineRule(
     "@thirtytech/yasml/match-export-parameters",
@@ -52,11 +31,11 @@ export function codeGeneratorYasmlPlugin(
         (!id.includes("node_modules") && id.endsWith(".ts")) ||
         id.endsWith(".tsx")
       ) {
-        const matchesPattern = patterns.some((pattern: any) =>
+        const matchesPattern = patterns ? patterns.some((pattern: any) =>
           typeof pattern === "string"
             ? code.includes(pattern)
             : pattern.test(code)
-        );
+        ) : true;
         if (matchesPattern) {
           const lintedResult = linter.verifyAndFix(
             code,
