4239cca2fb4c94d21b8d32b72af0b81085eefcb3
Jonathan Sheely <jsheely@thirtytech.net>
2026-06-06 08:41:47 -0400

    Made Property Selector more explicit on what it is shwoing

--- src/stories/PropertySelector.tsx
+++ src/stories/PropertySelector.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import { useSelector } from "./Context/MultiCounterSharedState";
 
 export function PropertySelector() {
@@ -5,17 +6,23 @@ export function PropertySelector() {
     counterOne: state.counterOne,
     update: state.setCounterTwo,
   }));
+  const ulRef = useRef<HTMLUListElement>(null);
   console.log("Initial Render. Will not render on click");
   return (
     <div>
+      <h3>Note: Clicking the button will not trigger a re-render. Update sets <code>counterTwo</code> not <code>counterOne</code>.</h3>
       <button
         onClick={() => {
           console.log("Click");
           update((prev) => prev + 1);
+          const li = document.createElement("li");
+          li.innerHTML = `[PropertySelector] Clicked ${new Date().getTime()}`;
+          ulRef.current?.appendChild(li);
         }}
       >
         {counterOne.toString()}
       </button>
+      <ul ref={ulRef}></ul>
     </div>
   );
 }
